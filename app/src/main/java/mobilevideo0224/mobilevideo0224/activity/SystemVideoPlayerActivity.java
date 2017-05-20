1c9e60061530e89994dbef67649268580db75480
tianxuewei1016 <93226539@qq.com>
2017-05-20 13:23:37 +0800

    播放器控制栏顶部,初始化数据

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemVideoPlayerActivity.java
@@ -4,22 +4,69 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import mobilevideo0224.mobilevideo0224.R;
 
 public class SystemVideoPlayerActivity extends AppCompatActivity {
 
-    private VideoView vv;
+    @InjectView(R.id.vv)
+    VideoView vv;
+    @InjectView(R.id.tv_name)
+    TextView tvName;
+    @InjectView(R.id.iv_battery)
+    ImageView ivBattery;
+    @InjectView(R.id.tv_systetime)
+    TextView tvSystetime;
+    @InjectView(R.id.btn_voice)
+    Button btnVoice;
+    @InjectView(R.id.seekbar_voice)
+    SeekBar seekbarVoice;
+    @InjectView(R.id.btn_swiche_player)
+    Button btnSwichePlayer;
+    @InjectView(R.id.ll_top)
+    LinearLayout llTop;
+    @InjectView(R.id.tv_currenttime)
+    TextView tvCurrenttime;
+    @InjectView(R.id.seekbar_video)
+    SeekBar seekbarVideo;
+    @InjectView(R.id.tv_duration)
+    TextView tvDuration;
+    @InjectView(R.id.btn_exit)
+    Button btnExit;
+    @InjectView(R.id.btn_pre)
+    Button btnPre;
+    @InjectView(R.id.btn_start_pause)
+    Button btnStartPause;
+    @InjectView(R.id.btn_next)
+    Button btnNext;
+    @InjectView(R.id.btn_swich_screen)
+    Button btnSwichScreen;
+    @InjectView(R.id.ll_bottom)
+    LinearLayout llBottom;
+    @InjectView(R.id.activity_system_video_player)
+    RelativeLayout activitySystemVideoPlayer;
+
     private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_video_player);
-        vv = (VideoView)findViewById(R.id.vv);
+        ButterKnife.inject(this);
+        vv = (VideoView) findViewById(R.id.vv);
 
         //得到播放地址
         uri = getIntent().getData();
@@ -54,5 +101,25 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
 
         //设置控制面板
         vv.setMediaController(new MediaController(this));
+    }
+
+    @OnClick({R.id.btn_voice, R.id.btn_swiche_player, R.id.btn_exit, R.id.btn_pre, R.id.btn_start_pause, R.id.btn_next, R.id.btn_swich_screen})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_voice:
+                break;
+            case R.id.btn_swiche_player:
+                break;
+            case R.id.btn_exit:
+                break;
+            case R.id.btn_pre:
+                break;
+            case R.id.btn_start_pause:
+                break;
+            case R.id.btn_next:
+                break;
+            case R.id.btn_swich_screen:
+                break;
+        }
     }
 }
