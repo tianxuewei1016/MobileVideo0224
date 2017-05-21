b8e63b6963e83e2c5f5b8ddf17c6b031dccb0442
tianxuewei1016 <93226539@qq.com>
2017-05-21 13:15:59 +0800

    视频seekBar进度更新

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemVideoPlayerActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemVideoPlayerActivity.java
@@ -3,12 +3,13 @@ package mobilevideo0224.mobilevideo0224.activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.MediaController;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -19,8 +20,14 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import mobilevideo0224.mobilevideo0224.R;
+import mobilevideo0224.mobilevideo0224.utils.Utils;
 
 public class SystemVideoPlayerActivity extends AppCompatActivity {
+
+    /**
+     * 视频进度更新
+     */
+    private static final int PROGRESS = 0;
 
     @InjectView(R.id.vv)
     VideoView vv;
@@ -59,7 +66,31 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
     @InjectView(R.id.activity_system_video_player)
     RelativeLayout activitySystemVideoPlayer;
 
+    private Utils utils;
+
     private Uri uri;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case PROGRESS:
+                    //得到当前的进度
+                    int currentPosition = vv.getCurrentPosition();
+                    //让SeekBar进度更新
+                    seekbarVideo.setProgress(currentPosition);
+
+                    //设置文本的播放速度
+                    tvCurrenttime.setText(utils.stringForTime(currentPosition));
+
+                    //循环发消息
+                    sendEmptyMessageDelayed(PROGRESS,1000);
+                    break;
+
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,14 +98,37 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_system_video_player);
         ButterKnife.inject(this);
         vv = (VideoView) findViewById(R.id.vv);
+
+        utils = new Utils();
         //得到播放地址
         uri = getIntent().getData();
+        setListener();
+
+
+        //设置播放的地址
+        vv.setVideoURI(uri);
+
+        //设置控制面板
+        //vv.setMediaController(new MediaController(this));
+    }
+
+    private void setListener() {
         //设置播放器的三个监听:播放准备好的监听,播放完成的监听,播放出错的监听
         vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             //底层准备播放完成的时候回调
             @Override
             public void onPrepared(MediaPlayer mp) {
+                //得到视频的总时间=长
+                int duration = vv.getDuration();
+                seekbarVideo.setMax(duration);
+
+                //设置文本总时间
+                tvDuration.setText(utils.stringForTime(duration));
+
                 vv.start();
+
+                //发消息开始更新播放速度
+                handler.sendEmptyMessage(PROGRESS);
             }
         });
 
@@ -95,11 +149,35 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
             }
         });
 
-        //设置播放的地址
-        vv.setVideoURI(uri);
+        seekbarVideo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            /**
+             * 状态变化的时候时候回调
+             * @param seekBar
+             * @param progress 当前改变的进度-要拖动到的位置
+             * @param fromUser 用户导致的改变true,否则false
+             */
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    vv.seekTo(progress);
+                }
+            }
 
-        //设置控制面板
-        vv.setMediaController(new MediaController(this));
+            /**
+             * 当手指按下的时候回调
+             * @param seekBar
+             */
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @OnClick({R.id.btn_voice, R.id.btn_swiche_player, R.id.btn_exit, R.id.btn_pre, R.id.btn_start_pause, R.id.btn_next, R.id.btn_swich_screen})
@@ -114,12 +192,12 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
             case R.id.btn_pre:
                 break;
             case R.id.btn_start_pause:
-                if(vv.isPlaying()) {
+                if (vv.isPlaying()) {
                     //暂停
                     vv.pause();
                     //按钮状态--播放
                     btnStartPause.setBackgroundResource(R.drawable.btn_start_selector);
-                }else{
+                } else {
                     //播放
                     vv.start();
                     //按钮状态--暂停
@@ -132,5 +210,12 @@ public class SystemVideoPlayerActivity extends AppCompatActivity {
             case R.id.btn_swich_screen:
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //把消息移除
+        handler.removeCallbacksAndMessages(null);
     }
 }
