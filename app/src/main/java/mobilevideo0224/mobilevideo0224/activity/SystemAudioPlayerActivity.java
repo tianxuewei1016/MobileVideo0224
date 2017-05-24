c187007d98c28900625854c3aebaa58502f0e79a
tianxuewei1016 <93226539@qq.com>
2017-05-24 20:47:02 +0800

    音乐播放器页面的完成

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
@@ -2,14 +2,68 @@ package mobilevideo0224.mobilevideo0224.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import mobilevideo0224.mobilevideo0224.R;
 
 public class SystemAudioPlayerActivity extends AppCompatActivity {
+
+    @InjectView(R.id.iv_icon)
+    ImageView ivIcon;
+    @InjectView(R.id.tv_artist)
+    TextView tvArtist;
+    @InjectView(R.id.tv_audioname)
+    TextView tvAudioname;
+    @InjectView(R.id.rl_top)
+    RelativeLayout rlTop;
+    @InjectView(R.id.tv_time)
+    TextView tvTime;
+    @InjectView(R.id.seekbar_audio)
+    SeekBar seekbarAudio;
+    @InjectView(R.id.btn_playmode)
+    Button btnPlaymode;
+    @InjectView(R.id.btn_pre)
+    Button btnPre;
+    @InjectView(R.id.btn_start_pause)
+    Button btnStartPause;
+    @InjectView(R.id.btn_next)
+    Button btnNext;
+    @InjectView(R.id.btn_lyric)
+    Button btnLyric;
+    @InjectView(R.id.ll_bottom)
+    LinearLayout llBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_audio_player);
+        ButterKnife.inject(this);
+
+
+    }
+
+    @OnClick({R.id.btn_playmode, R.id.btn_pre, R.id.btn_start_pause, R.id.btn_next, R.id.btn_lyric})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_playmode:
+                break;
+            case R.id.btn_pre:
+                break;
+            case R.id.btn_start_pause:
+                break;
+            case R.id.btn_next:
+                break;
+            case R.id.btn_lyric:
+                break;
+        }
     }
 }
