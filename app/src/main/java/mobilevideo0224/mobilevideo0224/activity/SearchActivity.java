98983ede5468ef534f73d3ae044b103f35f9fc0a
tianxuewei1016 <93226539@qq.com>
2017-05-27 13:32:59 +0800

    科大讯飞集成&实现语输入功能

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SearchActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SearchActivity.java
@@ -1,17 +1,35 @@
 package mobilevideo0224.mobilevideo0224.activity;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import mobilevideo0224.mobilevideo0224.R;
+import mobilevideo0224.mobilevideo0224.utils.JsonParser;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -23,7 +41,8 @@ public class SearchActivity extends AppCompatActivity {
     TextView tvGo;
     @InjectView(R.id.lv)
     ListView lv;
-
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +54,74 @@ public class SearchActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_voice:
+                //语言数据
+                showVoiceDialog();
                 break;
             case R.id.tv_go:
                 break;
         }
+    }
+
+    private void showVoiceDialog() {
+        //1.创建RecognizerDialog对象
+        RecognizerDialog mDialog = new RecognizerDialog(this, new MyInitListener());
+        //2.设置accent、language等参数
+        mDialog.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        mDialog.setParameter(SpeechConstant.ACCENT, "mandarin");
+        //若要将UI控件用于语义理解，必须添加以下参数设置，设置之后onResult回调返回将是语义理解
+        //结果
+        // mDialog.setParameter("asr_sch", "1");
+        // mDialog.setParameter("nlp_version", "2.0");
+        //3.设置回调接口
+        mDialog.setListener(new MyRecognizerDialogListener());
+        //4.显示dialog，接收语音输入
+        mDialog.show();
+    }
+    class MyRecognizerDialogListener implements RecognizerDialogListener {
+
+        @Override
+        public void onResult(RecognizerResult recognizerResult, boolean b) {
+            String resultString = recognizerResult.getResultString();
+            Log.e("TAG", "onResult--resultString==" + resultString);
+            printResult(recognizerResult);
+        }
+
+        @Override
+        public void onError(SpeechError speechError) {
+            Log.e("TAG", "onError==" + speechError.getMessage());
+        }
+    }
+
+    class MyInitListener implements InitListener {
+
+        @Override
+        public void onInit(int i) {
+            if (i == ErrorCode.SUCCESS) {
+                Toast.makeText(SearchActivity.this, "初始化成功", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void printResult(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+
+        String sn = null;
+        // 读取json结果中的sn字段
+        try {
+            JSONObject resultJson = new JSONObject(results.getResultString());
+            sn = resultJson.optString("sn");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mIatResults.put(sn, text);
+
+        StringBuffer resultBuffer = new StringBuffer();
+        for (String key : mIatResults.keySet()) {
+            resultBuffer.append(mIatResults.get(key));
+        }
+
+        etSousuo.setText(resultBuffer.toString());
+        etSousuo.setSelection(etSousuo.length());
     }
 }
--- app/src/main/java/mobilevideo0224/mobilevideo0224/app/MyApplication.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/app/MyApplication.java
@@ -2,6 +2,9 @@ package mobilevideo0224.mobilevideo0224.app;
 
 import android.app.Application;
 
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
@@ -19,5 +22,7 @@ public class MyApplication extends Application {
         //xUtils初始化
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+
+        SpeechUtility.createUtility(this, SpeechConstant.APPID +"=5928e7f4");
     }
 }
