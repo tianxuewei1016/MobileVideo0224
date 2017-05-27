e6946841441896ae910de0f1fa21d1c5107a2523
tianxuewei1016 <93226539@qq.com>
2017-05-27 13:17:47 +0800

    增加软件退出功能

--- app/src/main/java/mobilevideo0224/mobilevideo0224/MainActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/MainActivity.java
@@ -5,11 +5,14 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -136,5 +139,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.e("TAG", "onDestroy");
+    }
+
+    /**
+     * 是否已经退出--false
+     */
+    private boolean isExit = false;
+    /**
+     * 防止不小心点击退出
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(position != 0) {
+                rg_main.check(R.id.rb_local_video);
+                return true;
+            }else if(!isExit) {
+                Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                isExit = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                      isExit = false;
+                    }
+                },2000);
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
